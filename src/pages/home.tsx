4d0d8dd982b68894f5910bc0c96255aadc176381
Matheus Omena <omena.theus@live.com>
2025-02-12 20:37:53 -0300

    feat: create NewsOrg and The Guardian services

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,20 @@
 import { useNews } from '../services/news-service';
 
 export default function Home() {
-  const { nyTimesNews, isNyTimesDataLoading } = useNews();
+  const {
+    nyTimesNews,
+    isNyTimesDataLoading,
+    newsOrgNews,
+    isNewsOrgDataLoading,
+    theGuardianNews,
+    isTheGuardianDataLoading,
+  } = useNews();
 
   console.log('NY NEWS RETURN >>>', nyTimesNews);
+  console.log('News Org NEWS RETURN >>>', newsOrgNews);
+  console.log('The Guardian NEWS RETURN >>>', theGuardianNews);
 
-  if (isNyTimesDataLoading) return <p>Carregando notícias</p>;
+  if (isNyTimesDataLoading && isNewsOrgDataLoading && isTheGuardianDataLoading) return <p>Loading news</p>;
 
   return <p>All news</p>;
 }
--- src/services/news-service.ts
+++ src/services/news-service.ts
@@ -2,6 +2,8 @@ import { useQuery } from '@tanstack/react-query';
 import externalNewsApis from '../constants';
 import axios from 'axios';
 import { NyTimesApiResponse } from '../interfaces/nytimes-interface';
+import { NewsOrgApiResponse } from '../interfaces/neworgs-interface';
+import { TheGuardianApiResponse } from '../interfaces/theguardian-interface';
 
 export function useNews() {
   const { data: nyTimesData, isPending: isNyTimesDataLoading } = useQuery({
@@ -15,8 +17,34 @@ export function useNews() {
     queryKey: ['NYTimesNews'],
   });
 
+  const { data: newsOrgData, isPending: isNewsOrgDataLoading } = useQuery({
+    queryFn: async () => {
+      const { apiBaseURL, apiKey } = externalNewsApis.NewsOrg;
+      const endpoint = `${apiBaseURL}&apiKey=${apiKey}`;
+
+      const { data } = await axios.get<NewsOrgApiResponse>(endpoint);
+      return data?.articles;
+    },
+    queryKey: ['NewsOrgNews'],
+  });
+
+  const { data: theGuardianData, isPending: isTheGuardianDataLoading } = useQuery({
+    queryFn: async () => {
+      const { apiBaseURL, apiKey } = externalNewsApis.TheGuardian;
+      const endpoint = `${apiBaseURL}?api-key=${apiKey}`;
+
+      const { data } = await axios.get<TheGuardianApiResponse>(endpoint);
+      return data?.response?.results;
+    },
+    queryKey: ['TheGuardianNews'],
+  });
+
   return {
     nyTimesNews: nyTimesData,
     isNyTimesDataLoading,
+    newsOrgNews: newsOrgData,
+    isNewsOrgDataLoading,
+    theGuardianNews: theGuardianData,
+    isTheGuardianDataLoading,
   };
 }
