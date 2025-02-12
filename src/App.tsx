3abcda40bb4a6984107f72b846a4f165b63879bb
Matheus Omena <omena.theus@live.com>
2025-02-11 23:41:23 -0300

    build: add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Home from './pages/home';
+
 function App() {
   return (
-    <div className="bg-amber-600">
-      <div className="container mx-auto bg-amber-700 p-6">
-        <p>News aggregator</p>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
